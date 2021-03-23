594a15c36bafb446a5f61a55d1f2b252710111ed
Ian Boyes <igboyes@gmail.com>
2021-03-23 11:19:21 -0700

    Simplify workflow
    * Used rough fixture APIs for accessing data and remove direct database  access
    * Add TODOs for further integration required with virtool-workflow
    * Remove most custom fixtures

--- fixtures.py
+++ fixtures.py
@@ -1,26 +1,9 @@
 from virtool_workflow import fixture
-from virtool_workflow_runtime.config.configuration import db_name, db_connection_string
-from virtool_workflow_runtime.db import VirtoolDatabase
 
 
 @fixture
-def db():
-    return VirtoolDatabase(db_name(), db_connection_string())
-
-
-@fixture
-def sample_id(job_document):
-    return job_document["args"]["sample_id"]
-
-
-@fixture
-def sample_path(data_path, sample_id):
-    return data_path / f"samples/{sample_id}"
-
-
-@fixture
-async def document(db, sample_id):
-    return await db.samples.find_one(sample_id)
+def quality():
+    return dict()
 
 
 @fixture
--- workflow.py
+++ workflow.py
@@ -1,23 +1,22 @@
-import os
 import shutil
 
-from virtool_workflow import startup, step, cleanup
-import virtool_core.samples.utils
+from virtool_core.samples.utils import join_read_path
+from virtool_workflow import cleanup, step
 
 import utils
 from utils import parse_fastqc
 
 
 @step
-async def copy_files(params, data_path, run_in_executor, proc, db):
+async def fetch_reads(params, data_path, run_in_executor, proc, db):
     """
-    Copy the files from the files directory to the nascent sample directory.
+    Fetch the uploaded reads being used to create this sample.
+
+    TODO: Figure out pathing.
 
     """
     files = params["files"]
     sample_id = params["sample_id"]
-
-    paths = [data_path / "file" / file["id"] for file in files]
 
     sizes = await run_in_executor(
         utils.copy_files_to_sample,
@@ -26,30 +25,13 @@ async def copy_files(params, data_path, run_in_executor, proc, db):
         proc
     )
 
-    raw = list()
-
-    for index, file in enumerate(files):
-        name = f"reads_{index + 1}.fq.gz"
-
-        raw.append({
-            "name": name,
-            "download_url": f"/download/samples/{sample_id}/{name}",
-            "size": sizes[index],
-            "from": file,
-            "raw": True
-        })
-
-    await db.samples.update_one({"_id": sample_id}, {
-        "$set": {
-            "files": raw
-        }
-    })
-
 
 @step
 async def fastqc(params, sample, run_in_executor, run_subprocess, proc, work_path):
     """
     Runs FastQC on reads.
+
+    TODO: Figure out pathing and get rid of temp_sample_path.
 
     """
     fastqc_path = work_path / "fastqc"
@@ -60,49 +42,41 @@ async def fastqc(params, sample, run_in_executor, run_subprocess, proc, work_pat
         run_subprocess,
         proc,
         read_paths,
-        params["fastqc_path"]
+        fastqc_path
     )
 
-    qc = await run_in_executor(
+    sample.quality = await run_in_executor(
         parse_fastqc,
         params["fastqc_path"],
         params["temp_sample_path"]
     )
 
 
-
-
 @step
-async def upload(params, run_in_executor):
-    await run_in_executor(
+async def upload(params, run_in_executor, sample, quality):
+    """
+    Upload sample reads and artifacts and finalize the database record
+
+    TODO: Figure out pathing and uploading. This is not functional here.
+    TODO: Make sure file size is calculate in virtool-workflow
+
+    """
+    await sample.upload_reads(
         shutil.copytree,
         params["temp_sample_path"],
         params["sample_path"]
     )
 
-
-@step
-async def clean_watch(params, db):
-    """Remove the original read files from the files directory"""
-    file_ids = [f["id"] for f in params["files"]]
-    await db.files.delete_many({"_id": {"$in": file_ids}})
+    await sample.finalize()
 
 
 @cleanup
-async def delete_sample(params, db, run_in_executor):
-    await db.samples.delete_one({"_id": params["sample_id"]})
+async def delete_sample(sample):
+    """
+    Delete the unfinalized sample. On the server, this will trigger:
+    - removal of read upload reservations
+    - deletion of any sample files uploaded to server
+    - removal of sample database records
 
-    try:
-        await run_in_executor(shutil.rmtree, params["sample_path"])
-    except FileNotFoundError:
-        pass
-
-
-@cleanup
-async def release_files(params, db):
-    for file_id in params["files"]:
-        await db.files.update_many({"_id": file_id}, {
-            "$set": {
-                "reserved": False
-            }
-        })
+    """
+    await sample.delete()
