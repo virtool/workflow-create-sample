a284b92c835bb4a841e5c330cf00607dbd0abaf1
Blake Smith <blakeinvictoria@gmail.com>
2021-08-10 15:37:55 -0700

    Get `create_sample` workflow running in k3s

--- vt_workflow_create_sample/workflow.py
+++ vt_workflow_create_sample/workflow.py
@@ -1,8 +1,9 @@
+from pathlib import Path
 from types import SimpleNamespace
-from typing import Dict, Path
+from typing import Dict
 
-from virtool_core.data_model import Sample
-from virtool_workflow import fixture, FixtureScope, step, hooks
+from fixtures import fixture
+from virtool_workflow import hooks, step
 from virtool_workflow.api.samples import SampleProvider
 
 __package__ = "vt_workflow_create_sample"
@@ -15,9 +16,9 @@ def intermediate():
 
 @step
 async def download_raw_files(
+    intermediate,
     sample_provider: SampleProvider,
     input_files: Dict[str, Path],
-    scope: FixtureScope,
 ):
     """
     Download the read files which were uploaded by the user and
@@ -25,57 +26,47 @@ async def download_raw_files(
     """
     left = input_files.get("reads_1.fq.gz")
 
-    sample = await sample_provider.get()
+    intermediate.sample = await sample_provider.get()
 
-    # Set the sample path to be the download path
-    sample.path = left.parent
+    # Set the intermediate.sample path to be the download path
+    intermediate.sample.path = left.parent
 
-    scope["sample"] = sample
-
-    return "Raw reads for sample {sample.id} collected."
+    return "Raw reads for intermediate.sample {sample.id} collected."
 
 
 @step
 async def run_fastqc(
     fastqc,
-    sample: Sample,
-    scope: FixtureScope,
+    intermediate,
 ):
     """
     Run `fastqc` on the read files. Parse the output
     into a dictionary and add it to the scope.
     """
-    read_paths = [sample.left]
-    if sample.paired:
-        read_paths.append(sample.right)
+    read_paths = [intermediate.sample.path/"reads_1.fq.gz"]
+    if intermediate.sample.paired:
+        read_paths.append(intermediate.sample.path/"reads_1.fq.gz")
 
-    scope["quality"] = await fastqc(read_paths)
+    intermediate.quality = await fastqc(read_paths)
 
     return "Fastqc run completed."
 
 
 @step
-async def upload_read_files(sample: Sample, sample_provider: SampleProvider):
+async def upload_read_files(intermediate, sample_provider: SampleProvider):
     """Upload the read files."""
-    await sample_provider.upload(sample.left)
-    if sample.paired:
-        await sample_provider.upload(sample.right)
+    await sample_provider.upload(intermediate.sample.path/"reads_1.fq.gz")
+    if intermediate.sample.paired:
+        await sample_provider.upload(intermediate.sample.right/"reads_2.fq.gz")
 
 
 @step
-async def upload_quality(sample_provider: SampleProvider, quality: dict):
-    """
-    Upload the resulting quality to the sample record.
-    """
-    await sample_provider.finalize(quality)
+async def upload_quality(sample_provider: SampleProvider, intermediate):
+    """Upload the resulting quality to the sample record."""
+    await sample_provider.finalize(intermediate.quality)
 
 
 @hooks.on_failure
 async def delete_sample(sample_provider: SampleProvider):
-    """
-    Delete the sample in the case of a failure.
-
-    :param sample_provider: [description]
-    :type sample_provider: SampleProvider
-    """
+    """Delete the sample in the case of a failure."""
     await sample_provider.delete()
