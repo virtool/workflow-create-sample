237af281a674f6958186933fb7db4f03bf2680cb
Ian Boyes <igboyes@gmail.com>
2024-01-11 15:05:09 -0800

    fix: support `virtool-workflow==6.0.0-alpha`

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,22 @@
-FROM ghcr.io/virtool/workflow:5.4.2
-WORKDIR /app
-COPY fixtures.py workflow.py ./
+FROM python:3.10-buster as jre
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends default-jre && \
+    rm -rf /var/lib/apt/lists/* && \
+    apt-get clean
+
+FROM python:3.10-buster as poetry
+RUN curl -sSL https://install.python-poetry.org | python -
+ENV PATH="/root/.local/bin:${PATH}"
+COPY pyproject.toml poetry.lock workflow.py ./
+RUN poetry install
+
+FROM jre as base
+COPY --from=ghcr.io/virtool/workflow-tools:2.0.1 /opt/fastqc /opt/fastqc
+RUN chmod ugo+x /opt/fastqc/fastqc && \
+    ln -fs /opt/fastqc/fastqc /usr/local/bin/fastqc
+RUN curl -sSL https://install.python-poetry.org | python -
+ENV PATH="/root/.local/bin:${PATH}"
+COPY pyproject.toml poetry.lock workflow.py ./
+RUN poetry install
+RUN poetry export > requirements.txt
+RUN pip install -r requirements.txt
--- workflow.py
+++ workflow.py
@@ -1,26 +1,47 @@
+import asyncio
+from pathlib import Path
 from types import SimpleNamespace
 
+from pyfixtures import fixture
 from virtool_workflow import hooks, step
-from virtool_workflow.api.samples import SampleProvider
+from virtool_workflow.analysis.fastqc import FastQCRunner
+from virtool_workflow.analysis.utils import ReadPaths
+from virtool_workflow.data.samples import WFNewSample
+
+
+@fixture
+def intermediate() -> SimpleNamespace:
+    return SimpleNamespace()
+
+
+@fixture
+def read_paths(new_sample: WFNewSample) -> ReadPaths:
+    if len(new_sample.uploads) == 1:
+        return (new_sample.uploads[0].path,)
+
+    return new_sample.uploads[0].path, new_sample.uploads[1].path
 
 
 @step(name="Run FastQC")
 async def run_fastqc(
-    fastqc,
+    fastqc: FastQCRunner,
     intermediate: SimpleNamespace,
-    read_files,
+    read_paths: ReadPaths,
+    work_path: Path,
 ):
     """
     Run `fastqc` on the read files.
 
     Parse the output into a dictionary and add it to the scope.
     """
-    intermediate.quality = await fastqc(read_files)
+    output_path = work_path / "fastqc"
+    await asyncio.to_thread(output_path.mkdir)
+    intermediate.quality = await fastqc(read_paths, output_path)
 
 
 @step
 async def finalize(
-    intermediate: SimpleNamespace, sample_provider: SampleProvider, read_files
+    intermediate: SimpleNamespace, new_sample: WFNewSample, read_paths: ReadPaths
 ):
     """
     Save the sample data in Virtool.
@@ -28,13 +49,14 @@ async def finalize(
     * Upload the read files to the sample file endpoints.
     * POST the JSON quality data to sample endpoint.
     """
-    for file in read_files:
-        await sample_provider.upload(file)
+    for i, path in enumerate(read_paths):
+        new_path = await asyncio.to_thread(path.rename, f"reads_{i + 1}.fq.gz")
+        await new_sample.upload(new_path, "fastq")
 
-    await sample_provider.finalize(intermediate.quality)
+    await new_sample.finalize(intermediate.quality)
 
 
 @hooks.on_failure
-async def delete_sample(sample_provider: SampleProvider):
+async def delete_sample(new_sample: WFNewSample):
     """Delete the sample in the case of a failure."""
-    await sample_provider.delete()
+    await new_sample.delete()
